449ce221ba620d6edccf3b7b9aa7214c12bc6a97
Potat05 <williamkoerber6@gmail.com>
2023-07-27 14:10:28 -0500

    gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        // adapter-static is used for github pages, see https://kit.svelte.dev/docs/adapter-static.
 		adapter: adapter()
 	}
 };
